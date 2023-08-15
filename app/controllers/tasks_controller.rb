c4eb571b89c0faed839517426b8266463a458b8e
Diogo Pinto <diogo.alvespinto@gmail.com>
2023-08-15 13:42:11 +0100

    generated edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,42 @@
 class TasksController < ApplicationController
-  def index
-  end
+  before_action :set_task, only: ['show', 'edit', 'update', 'destroy']
+
+  def index; end
 
   def list
     @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    # we give the new params to the insatnce
+    # we save the changes to the DB
+    @task.update(task_params)
+    # redirect the user to the show page
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
   get 'tasks/list', to: 'tasks#list'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  post 'tasks', to: 'tasks#create'
 end
