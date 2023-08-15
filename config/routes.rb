0043cb1b02fa54aae199bbb6458509a595b5c6eb
Diogo Pinto <diogo.alvespinto@gmail.com>
2023-08-15 14:54:23 +0100

    refactored and front-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  post 'tasks', to: 'tasks#create'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # post 'tasks', to: 'tasks#create'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
